fec2d9c99dbf5f7e8103bc8284539cc244705749
Mahkamov Ahadjon <ahadjonmahkamov111@gmail.com>
2022-12-07 15:49:52 +0500

    crud end

--- src/App.js
+++ src/App.js
@@ -1,17 +1,64 @@
 import './App.css';
-import { useSelector } from 'react-redux'
+import { useState } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import {addUser , deleteUser, updateUsername} from './features/Users';
 
 
 function App() {
+
+  const dispatch = useDispatch();
+  const userList = useSelector((state) => state.users.value)
+  
+  const [ name , setName] = useState("");
+  const [ username , setUsername] = useState("");
+  const [ newUsername , setNewUsername] = useState("");
+
+  
   return (
     <div className='App'>
     {""}
     <div className='addUser'>
-      <input type="text" placeholder='name'/>
-      <input type="text" placeholder='username'/>
-      <button>Add User</button>
+      <input 
+        type="text" 
+        placeholder='name' 
+        onChange={(event) => {setName(event.target.value)} } 
+      />
+      <input 
+        type="text" 
+        placeholder='username' 
+        onChange={(event) => {setUsername(event.target.value)} } 
+      />
+
+      <button 
+        onClick={() => {dispatch(addUser({id: userList[userList.length - 1].id + 1, name, username}))}} >
+          Add User
+      </button>
     </div>
-    <div className='displayUsers'></div>
+    <div className='displayUsers'>
+      {userList.map((user) => {
+        return(
+          <div className='users-box'>
+            <h3>{user.name}</h3>  
+            <h3>{user.username}</h3>
+            <input 
+              type="text" 
+              placeholder='new username' 
+              onChange={(event) => {
+                setNewUsername(event.target.value);
+              }} 
+            />
+            <button onClick={() => {dispatch(updateUsername({id: user.id ,username: newUsername}))}}>Updete username</button>
+            <button 
+              onClick={(e) => {
+                dispatch(deleteUser({id: user.id}));
+              }}
+            >
+              Delete user
+            </button>
+          </div>  
+        ) 
+      })}
+    </div>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -4,10 +4,12 @@ import App from './App';
 import {configureStore} from '@reduxjs/toolkit';
 import { Provider } from 'react-redux';
 
+import usersReducer from './features/Users';
+
 
 const store = configureStore({
   reducer: {
-    //users: ,
+    users: usersReducer,
   }
 })
 
